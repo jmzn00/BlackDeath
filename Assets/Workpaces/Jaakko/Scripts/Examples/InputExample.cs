45cd043129ceeadc368e58719b94ace341bb48df
Jaakko Mansikka <mansikkajaakko13@gmail.com>
2026-03-02 11:49:14 +0200

    Input Example

--- Assets/Workpaces/Jaakko/Scripts/Examples/InputExample.cs
+++ Assets/Workpaces/Jaakko/Scripts/Examples/InputExample.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class TestScript : MonoBehaviour
+public class InputExample : MonoBehaviour
 {
     private InputManager m_inputManager;
+    private InputSystem_Actions m_inputActions;
+    
     private void Start()
     {
         m_inputManager = GameManager.I.Input;
+
         m_inputManager.InputActions.Player.Jump.performed += ctx
             =>  Debug.LogWarning("Jump Pressed");
+
+        m_inputActions = m_inputManager.InputActions;
+        m_inputActions.Player.Jump.performed += ctx =>
+        {
+            Debug.LogError("Jump Pressed");
+        };
     }
     private void Update()
     {
