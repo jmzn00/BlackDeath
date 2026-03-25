7bb7cfbb46511223843ef32529a5234bc3740aae
lusikkadev <tatuketonen@gmail.com>
2026-03-25 13:08:00 +0200

    Started Combat Effect Manager

---
+++ Assets/Workpaces/Jaakko/Scripts/Combat/Effects/CombatEffectManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CombatEffectManager : MonoBehaviour
+{
+    [SerializeField] GameObject damagePopUpPrefab;
+    [SerializeField] GameObject particleEffect;
+    
+    // All effects triggered by listening to combat events
+
+
+}
