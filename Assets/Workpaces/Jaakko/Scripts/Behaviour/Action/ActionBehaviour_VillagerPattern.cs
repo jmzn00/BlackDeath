615784ed7a8b15cd3fff4d2c87e09542d2b8b262
Jaakko Mansikka <mansikkajaakko13@gmail.com>
2026-04-09 14:30:56 +0300

    VillagerPattern

---
+++ Assets/Workpaces/Jaakko/Scripts/Behaviour/Action/ActionBehaviour_VillagerPattern.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ai/ActionBehaviours/Villager/VillagerPattern")]
+public class ActionBehaviour_VillagerPattern : AIActionBehaviour 
+{    
+    public override float Evaluate(CombatActor actor, List<CombatActor> participants
+        , out CombatAction selectedAction)
+    {
+        selectedAction = null;
+
+        AICombatActor ai = actor as AICombatActor;
+        if (ai == null)
+        {
+            Debug.LogWarning("Actor is not AICombatActor on VillagerPattern");
+            return -1f;
+        }
+
+        if (ai.Actions == null || ai.Actions.Count == 0)
+            return -1f;
+
+        int startIndex = ai.GetPatternIndex();
+        int count = ai.Actions.Count;
+
+        CombatAction skipAction = null;
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = (startIndex + i) % count;
+            var action = ai.Actions[index];
+
+            if (action is SkipTurnAction)
+            {
+                skipAction = action;
+                continue;
+            }
+
+            if (action.CanExecute(ai, out string reason))
+            {
+                selectedAction = action;
+
+                ai.UpdatePatternIndex((index + 1) % count);
+
+                return selectedAction.baseDamage;
+            }
+            else
+            {
+                Debug.Log($"AI cannot perform {action.actionName}: {reason}");
+            }
+        }
+
+        if (skipAction != null)
+        {
+            selectedAction = skipAction;
+
+            ai.UpdatePatternIndex((startIndex + 1) % count);
+
+            return 0f;
+        }
+        return -1f;
+    }
+}
